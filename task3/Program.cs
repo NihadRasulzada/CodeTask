a7e7230477369def0514b4b09023820e9dfa312f
NihadRasulzada <62886261+NihadRasulzada@users.noreply.github.com>
2025-03-05 06:55:42 +0400

    Update Program.cs

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,11 +1,11 @@
-﻿
+
 Console.Write("Maaşi daxil edin: ");
 double maas = double.Parse(Console.ReadLine());
 
 Console.Write("Kredit deyerini daxil edin: ");
 double kredit = double.Parse(Console.ReadLine());
 
-if (kredit < maas * 0.6)
+if (kredit < maas * 0.6 * 12)
 {
     Console.WriteLine("Kredit goture bilmezsiniz!");
 }
